4e12876f3066e0f52fb01e2ac64373c742928e36
Sven Gothel <sgothel@jausoft.com>
2013-01-30 21:09:47 +0100

    Jenkins: Add OSX/Java7 node (where build is performed w/ source/target 1.6 incl. Java6 rt.jar)

---
+++ jenkins-server-slave-setup/nodes/sgothel-macosx64-nv320m-012_013/start-ssh-jenkins.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+
+cd /Users/jogamp/jenkins
+
+scp chuckslave@jogamp.org:/srv/jenkins/war/WEB-INF/slave.jar .
+
+function connect_12 {
+  . /opt-share/etc/profile.ant
+  JAVA_HOME=`/usr/libexec/java_home -version 1.6`
+  PATH=$JAVA_HOME/bin:$PATH
+  export JAVA_HOME PATH
+  export SOURCE_LEVEL=1.6
+  export TARGET_LEVEL=1.6
+  export TARGET_RT_JAR=/opt-share/jre1.6.0_30/lib/rt.jar
+  java -version
+  which git
+  sshpid=
+  while true ; do
+    if [ ! -z "$sshpid" ] ; then
+	kill -9 $sshpid
+    fi
+    ssh -o "ServerAliveInterval 30" -o "ServerAliveCountMax 5" -o "TCPKeepAlive yes" chuckslave@jogamp.org -L 6012:localhost:5555 -N &
+    sshpid=$!
+    java -server -Xmx1024m -jar slave.jar -jnlpUrl https://jogamp.org/chuck/computer/macosx64-NV320M-10_6-jogamp-x64-sgothel-012/slave-agent.jnlp
+  done
+}
+
+function connect_13 {
+  . /opt-share/etc/profile.ant
+  JAVA_HOME=`/usr/libexec/java_home -version 1.7`
+  PATH=$JAVA_HOME/bin:$PATH
+  export JAVA_HOME PATH
+  export SOURCE_LEVEL=1.6
+  export TARGET_LEVEL=1.6
+  export TARGET_RT_JAR=/opt-share/jre1.6.0_30/lib/rt.jar
+  java -version
+  which git
+  sshpid=
+  while true ; do
+    if [ ! -z "$sshpid" ] ; then
+	kill -9 $sshpid
+    fi
+    ssh -o "ServerAliveInterval 30" -o "ServerAliveCountMax 5" -o "TCPKeepAlive yes" chuckslave@jogamp.org -L 6013:localhost:5555 -N &
+    sshpid=$!
+    java -server -Xmx1024m -jar slave.jar -jnlpUrl https://jogamp.org/chuck/computer/macosx64-NV320M-10_7-jogamp-x64-jre7-sgothel-013/slave-agent.jnlp
+  done
+}
+
+connect_12 > macosx64-NV320M-10_6-jogamp-x64-sgothel-012.log 2>&1 &
+connect_13 > macosx64-NV320M-10_6-jogamp-x64-jre7-sgothel-013.log 2>&1 &
+disown $!
+
