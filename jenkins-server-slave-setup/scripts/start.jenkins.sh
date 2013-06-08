ea8e82f8cb83a8732af7133d8ca0986af6717101
Sven Gothel <sgothel@jausoft.com>
2013-06-08 04:59:45 +0200

    Updated jenkins start/init scripts. Align our debian init script w/ the one we used for redhat

--- jenkins-server-slave-setup/scripts/start.jenkins.sh
+++ jenkins-server-slave-setup/scripts/start.jenkins.sh
@@ -3,5 +3,6 @@ export JENKINS_HOME=/srv/jenkins
 export JENKINS_WAR=$JENKINS_HOME/jenkins.war
 export JENKINS_LOG=$JENKINS_HOME/jenkins.log
 JAVA=$JAVA_HOME/bin/java
-nohup nice $JAVA -jar $JENKINS_WAR --httpPort=8080 --prefix=/chuck > $JENKINS_LOG 2>&1 &
+nohup nice $JAVA -server -Xmx1024m -jar $JENKINS_WAR --httpPort=8080 --prefix=/chuck > $JENKINS_LOG 2>&1 &
+# nohup nice $JAVA -server -Xmx1024m -jar $JENKINS_WAR --httpPort=-1 --httpsPort=8080 --prefix=/chuck > $JENKINS_LOG 2>&1 &
 
