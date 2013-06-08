a2a277cf4c081fc961a02df2305a7bb4ce8be971
Sven Gothel <sgothel@jausoft.com>
2013-06-08 05:18:10 +0200

    Use dedicated jre6 for jenkins ..

--- jenkins-server-slave-setup/scripts/start.jenkins.sh
+++ jenkins-server-slave-setup/scripts/start.jenkins.sh
@@ -2,7 +2,11 @@
 export JENKINS_HOME=/srv/jenkins
 export JENKINS_WAR=$JENKINS_HOME/jenkins.war
 export JENKINS_LOG=$JENKINS_HOME/jenkins.log
+
+#export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:bin/javac::")
+export JAVA_HOME=/opt-linux-x86_64/jre6
 JAVA=$JAVA_HOME/bin/java
+
 nohup nice $JAVA -server -Xmx1024m -jar $JENKINS_WAR --httpPort=8080 --prefix=/chuck > $JENKINS_LOG 2>&1 &
 # nohup nice $JAVA -server -Xmx1024m -jar $JENKINS_WAR --httpPort=-1 --httpsPort=8080 --prefix=/chuck > $JENKINS_LOG 2>&1 &
 
