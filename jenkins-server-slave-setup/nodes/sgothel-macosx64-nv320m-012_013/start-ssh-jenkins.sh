25b81ec3b218158a592108c2202d5287acb816fc
Sven Gothel <sgothel@jausoft.com>
2014-06-11 00:30:27 +0200

    OSX build node: remove gluegen-xcode_clang.properties usage, xcode.clang is default now

--- jenkins-server-slave-setup/nodes/sgothel-macosx64-nv320m-012_013/start-ssh-jenkins.sh
+++ jenkins-server-slave-setup/nodes/sgothel-macosx64-nv320m-012_013/start-ssh-jenkins.sh
@@ -15,8 +15,6 @@ function connect_12 {
   export TARGET_LEVEL=1.6
   export TARGET_RT_JAR=/opt-share/jre1.6.0_30/lib/rt.jar
   
-  export GLUEGEN_PROPERTIES_FILE="/Users/jogamp/gluegen-xcode_clang.properties"
-
   export JOGAMP_JAR_CODEBASE="Codebase: *.jogamp.org"
 
   java -version
@@ -40,8 +38,6 @@ function connect_13 {
   export SOURCE_LEVEL=1.6
   export TARGET_LEVEL=1.6
   export TARGET_RT_JAR=/opt-share/jre1.6.0_30/lib/rt.jar
-
-  export GLUEGEN_PROPERTIES_FILE="/Users/jogamp/gluegen-xcode_clang.properties"
 
   export JOGAMP_JAR_CODEBASE="Codebase: *.jogamp.org"
 
