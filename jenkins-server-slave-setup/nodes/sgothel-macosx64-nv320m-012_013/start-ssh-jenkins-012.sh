005534be805f2de7d5902ccb9be17a4c8440feda
Sven Gothel <sgothel@jausoft.com>
2018-12-10 13:07:52 +0100

    Update Jenkins Node: MacOSX (amd64) running Oracle JDK 8

--- jenkins-server-slave-setup/nodes/sgothel-macosx64-nv320m-012_013/start-ssh-jenkins-012.sh
+++ jenkins-server-slave-setup/nodes/sgothel-macosx64-nv320m-012_013/start-ssh-jenkins-012.sh
@@ -2,14 +2,13 @@
 
 cd /Users/jogamp/jenkins
 
-scp chuckslave@jogamp.org:/srv/jenkins/war/WEB-INF/slave.jar slave-012.jar
+#scp chuckslave@jogamp.org:/srv/jenkins/war/WEB-INF/slave.jar slave-012.jar
+curl -o agent-012.jar https://jogamp.org/chuck/jnlpJars/agent.jar
 
-. /opt-share/etc/profile.ant
-JAVA_HOME=`/usr/libexec/java_home -version 1.6`
+. ./profile.ant
+JAVA_HOME=`/usr/libexec/java_home`
 PATH=$JAVA_HOME/bin:$PATH
 export JAVA_HOME PATH
-#JAVA7_EXE=`/usr/libexec/java_home -version 1.7.0_12`/bin/java
-#export JAVA7_EXE
 export SOURCE_LEVEL=1.6
 export TARGET_LEVEL=1.6
 export TARGET_RT_JAR=/opt-share/jre1.6.0_30/lib/rt.jar
@@ -21,5 +20,5 @@ which git
 ssh -o "ServerAliveInterval 30" -o "ServerAliveCountMax 5" -o "TCPKeepAlive yes" chuckslave@jogamp.org -L 6012:localhost:5555 -N &
 SSHPID=$!
 echo Launched SSH $SSHPID
-java -server -Xmx1024m -jar slave-012.jar -jnlpUrl https://jogamp.org/chuck/computer/macosx64-NV320M-10_6-jogamp-x64-sgothel-012/slave-agent.jnlp
+java -server -Xmx1024m -XX:+UseCompressedOops -jar agent-012.jar -jnlpUrl https://jogamp.org/chuck/computer/macosx64-NV320M-10_6-jogamp-x64-sgothel-012/slave-agent.jnlp
 
--- jenkins-server-slave-setup/nodes/sgothel-macosx64-nv320m-012_013/start-ssh-jenkins-013.sh
+++ jenkins-server-slave-setup/nodes/sgothel-macosx64-nv320m-012_013/start-ssh-jenkins-013.sh
@@ -2,9 +2,10 @@
 
 cd /Users/jogamp/jenkins
 
-scp chuckslave@jogamp.org:/srv/jenkins/war/WEB-INF/slave.jar slave-013.jar
+#scp chuckslave@jogamp.org:/srv/jenkins/war/WEB-INF/slave.jar slave-013.jar
+curl -o agent-013.jar https://jogamp.org/chuck/jnlpJars/agent.jar
 
-. /opt-share/etc/profile.ant
+. ./profile.ant
 JAVA_HOME=`/usr/libexec/java_home -version 1.8`
 PATH=$JAVA_HOME/bin:$PATH
 export JAVA_HOME PATH
@@ -19,6 +20,6 @@ which git
 ssh -o "ServerAliveInterval 30" -o "ServerAliveCountMax 5" -o "TCPKeepAlive yes" chuckslave@jogamp.org -L 6013:localhost:5555 -N &
 SSHPID=$!
 echo Launched SSH $SSHPID
-java -server -Xmx1024m -jar slave-013.jar -jnlpUrl https://jogamp.org/chuck/computer/macosx64-NV320M-10_7-jogamp-x64-jre7-sgothel-013/slave-agent.jnlp
+java -server -Xmx1024m -XX:+UseCompressedOops -jar agent-013.jar -jnlpUrl https://jogamp.org/chuck/computer/macosx64-NV320M-10_7-jogamp-x64-jre8-sgothel-013/slave-agent.jnlp
 
 
