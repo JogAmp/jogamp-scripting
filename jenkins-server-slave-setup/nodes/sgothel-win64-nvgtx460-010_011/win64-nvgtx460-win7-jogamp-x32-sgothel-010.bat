f8e7115322ac11784cf4457240c63c677348a70e
Sven Gothel <sgothel@jausoft.com>
2019-04-03 05:27:37 +0200

    Windows note 10/11: Use AdoptOpenJDK OpenJDK8 Build 8u202-b08

--- jenkins-server-slave-setup/nodes/sgothel-win64-nvgtx460-010_011/win64-nvgtx460-win7-jogamp-x32-sgothel-010.bat
+++ jenkins-server-slave-setup/nodes/sgothel-win64-nvgtx460-010_011/win64-nvgtx460-win7-jogamp-x32-sgothel-010.bat
@@ -1,5 +1,5 @@
-set J2RE_HOME=c:\jre1.8.0_121_x32
-set JAVA_HOME=c:\jdk1.8.0_121_x32
+set J2RE_HOME=c:\jre8u202-b08_x32
+set JAVA_HOME=c:\jdk8u202-b08_x32
 set ANT_PATH=C:\apache-ant-1.9.4
 set GIT_PATH=C:\cygwin\bin
 set SEVENZIP=C:\Program Files\7-Zip
