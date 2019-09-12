831794a8b50b6506a633d1b9e438f166bd0f935e
Sven Gothel <sgothel@jausoft.com>
2019-09-12 15:40:59 +0200

    Update to latest Cygwin 64bit 3.0.7

--- jenkins-server-slave-setup/cygwin-sshd/Cygwin-sshd.bat
+++ jenkins-server-slave-setup/cygwin-sshd/Cygwin-sshd.bat
@@ -1,5 +1,6 @@
 @echo off
 c:
-chdir c:\cygwin\bin
-bash --login -c /usr/sbin/sshd
-bash --login -c /usr/sbin/sshd
+chdir c:\cygwin64\bin
+REM bash --login -c "/usr/sbin/sshd ; /home/jogamp/jenkins/start-ssh-jenkins.sh"
+bash --login -c "/usr/sbin/sshd"
+
