e121e8a23b4af0d4ccc76ed4d5751033ee713e0c
Sven Gothel <sgothel@jausoft.com>
2020-01-02 02:26:59 +0100

    funcs_promotions.sh: prom_verify_artifacts shall ignore the build.sha.natives

--- jenkins-builds/funcs_promotion.sh
+++ jenkins-builds/funcs_promotion.sh
@@ -56,9 +56,11 @@ function prom_verify_artifacts() {
     shift
 
     local OK=0
-    diff -w $artia $artib && OK=1
+    grep -v ".build.sha.natives" $artia > $artia.no-build_sha_natives
+    grep -v ".build.sha.natives" $artib > $artib.no-build_sha_natives
+    diff -w $artia.no-build_sha_natives $artib.no-build_sha_natives_natives && OK=1
     if [ $OK -eq 0 ] ; then
-        echo "ERROR: $name artifacts differ $artia and $artib"
+        echo "ERROR: $name artifacts differ $artia and $artib no-build_sha_natives"
     fi
 }
 
