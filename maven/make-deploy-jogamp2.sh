5d67d2b7981d8e37c2814cbdfc0a4215fffb6a34
Sven Gothel <sgothel@jausoft.com>
2023-01-24 17:25:15 +0100

    maven: Add jogamp deploy script

---
+++ maven/make-deploy-jogamp2.sh
@@ -0,0 +1,12 @@
+
+. /opt-share/etc/profile.maven
+
+export REPOSITORY_URL="file:///srv/www/jogamp.org/deployment/maven/"
+#export REPOSITORY_URL="file:///srv/www/jogamp.org/deployment/test/maven01/"
+#export REPOSITORY_URL="scpexe://jogamp.org/srv/www/jogamp.org/deployment/maven/"
+#export REPOSITORY_URL="scpexe://jogamp.org/srv/www/jogamp.org/deployment/test/maven01/"
+export REPOSITORY_ID="jogamp-mirror"
+
+./make-deploy.sh $*
+#./make-deploy-one.sh jogl-all-android $*
+#./make-deploy-one.sh gluegen-rt-android $*
