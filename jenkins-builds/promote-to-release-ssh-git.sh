ab8addec293a1a5864dd9ff5986d12cdac709b62
Sven Gothel <sgothel@jausoft.com>
2023-02-01 18:05:30 +0100

    jenkins-builds/promote-to-release-ssh-git.sh: Add the maven make-all-jogamp.sh deployment step and log to file; Drop ssh-scp variant.

--- jenkins-builds/promote-to-release-ssh-git.sh
+++ jenkins-builds/promote-to-release-ssh-git.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 
-version=v2.4.0-rc-20200104
+version=v2.4.0-rc-20230201
 rootdir=/srv/www/jogamp.org/deployment
-adir=archive/master/gluegen_930-joal_652-jogl_1493-jocl_1137
+adir=archive/master/gluegen_951-joal_669-jogl_1518-jocl_1158
 sdir=archive/rc
 urlb=https://jogamp.org/deployment
+
+logfile=`basename $0 .sh`.log
+
+function deploy_it() {
 
 ssh jogamp@jogamp.org "\
     cd /home/jogamp/builds ; \
@@ -22,3 +26,12 @@ scp jogamp@jogamp.org:$rootdir/$sdir/$version/sha512sum.txt .
 gpg --output sha512sum.txt.sig --detach-sig sha512sum.txt
 gpg --verify sha512sum.txt.sig sha512sum.txt && \
 scp sha512sum.txt.sig jogamp@jogamp.org:$rootdir/$sdir/$version/
+
+ssh jogamp@jogamp.org "\
+    cd /home/jogamp/builds/jogamp-scripting/maven ; \
+    ./make-all-jogamp.sh $rootdir/$sdir/$version/archive/jogamp-all-platforms.7z $version ; \
+"
+
+}
+
+deploy_it 2>&1 | tee ${logfile}
