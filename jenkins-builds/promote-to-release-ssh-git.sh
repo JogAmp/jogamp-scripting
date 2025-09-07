9b5d33e028189f0c0e61f7f147e9519fa189d04f
Sven Göthel <sgothel@jausoft.com>
2025-09-08 00:12:17 +0200

    jenkins-builds/promote-to-release-ssh-git.sh: Adopt to launching maven deploy from local workstation
    We use local gpg keys from our local workstation and deploy to jogamp.org mirror and maven central.

--- jenkins-builds/promote-to-release-ssh-git.sh
+++ jenkins-builds/promote-to-release-ssh-git.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-plain_version=2.4.0
+#plain_version=2.6.0-rc-20250827
+plain_version=2.6.0
 version=v${plain_version}
 rootdir=/srv/www/jogamp.org/deployment
-adir=archive/master/gluegen_952-joal_670-jogl_1519-jocl_1159
+adir=archive/master/gluegen_987-joal_698-jogl_1548-jocl_1191
 sdir=archive/rc
 urlb=https://jogamp.org/deployment
 
@@ -28,10 +29,21 @@ gpg --output sha512sum.txt.sig --detach-sig sha512sum.txt
 gpg --verify sha512sum.txt.sig sha512sum.txt && \
 scp sha512sum.txt.sig jogamp@jogamp.org:$rootdir/$sdir/$version/
 
-ssh jogamp@jogamp.org "\
-    cd /home/jogamp/builds/jogamp-scripting/maven ; \
-    ./make-all-jogamp.sh $rootdir/$sdir/$version/archive/jogamp-all-platforms.7z ${plain_version} ; \
-"
+# we deploy maven from our local workstation
+# to maven central and jogamp.org mirror
+#
+#ssh jogamp@jogamp.org "\
+#    cd /home/jogamp/builds/jogamp-scripting/maven ; \
+#    ./make-all-jogamp.sh $rootdir/$sdir/$version/archive/jogamp-all-platforms.7z ${plain_version} ; \
+#"
+
+scp jogamp@jogamp.org:$rootdir/$sdir/$version/archive/jogamp-all-platforms.7z ../maven
+echo "#!/bin/sh" > deploy_maven.sh
+echo "cd ../maven" >> deploy_maven.sh
+echo "./make-all-jogamp.sh jogamp-all-platforms.7z ${plain_version}" >> deploy_maven.sh
+echo "./make-deploy-sonatype.sh ${plain_version}" >> deploy_maven.sh
+echo "deploy_maven.sh generated, consider using it"
+cat deploy_maven.sh
 
 }
 
